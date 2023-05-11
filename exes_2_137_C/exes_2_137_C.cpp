92c3aba3c37fab9c7a9551403d4919810899d034
LingGrani <luthfiangger2510@gmail.com>
2023-05-11 15:12:28 +0700

    Menghapus variable found

--- exes_2_137_C/exes_2_137_C.cpp
+++ exes_2_137_C/exes_2_137_C.cpp
@@ -4,7 +4,6 @@ using namespace std;
 int ling[33] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32}; //array of integers to hold values
 int n = 33;
 int elmt_wants;
-int found;
 int lower = 0;
 int upper = n - 1;
 int mid;
